3be3a6232e2de63d791c05c34db9fbd75239ee24
Mikhail <mikhail@volt.expert>
2019-02-11 13:03:24 +0300

    Пустое консольное приложение

---
+++ LearnCSharp/LearnCSharp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace LearnCSharp
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("fdfsdf");
+            Console.ReadLine();
+            
+        }
+    }
+}
